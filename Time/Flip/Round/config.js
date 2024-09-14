2d9e4ae2d950558cea9f328006be40d78a33d6d1
Careful S.  小心超人 <138026126+Ckarefulon@users.noreply.github.com>
2024-09-14 11:09:26 +0800

    Add files via upload

---
+++ Time/Flip/Round/config.js
@@ -0,0 +1,11 @@
+﻿var config={
+
+    'language_type':2,
+    'font_color':'#7F7F7F',
+    'pointer_color':'#7F7F7F',
+    'background_color':'#7F7F7F',
+    'background_style':2,
+    'background_picture':'bg.jpg',
+    'background_video':'3.mp4',
+    'background_color':'#000000',
+}
