7c9353155b957191f6895feb28c9b1e8eb087b3c
Careful S.  小心超人 <138026126+Ckarefulon@users.noreply.github.com>
2024-09-15 22:18:56 +0800

    Update config.js

--- Time/Round/config.js
+++ Time/Round/config.js
@@ -1,9 +1,9 @@
-﻿var config={
+var config={
 
     'language_type':2,
-    'font_color':'#7F7F7F',
-    'pointer_color':'#7F7F7F',
-    'background_color':'#7F7F7F',
+    'font_color':'#24F0EA00',
+    'pointer_color':'#24F0EA00',
+    'background_color':'#24F0EA00',
     'background_style':2,
     'background_picture':'bg.jpg',
     'background_video':'3.mp4',
